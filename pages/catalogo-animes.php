3e86a4e821fef2c258f3999813808a563a246ac9
Sergio Gil Jiménez <sgil@AZMotor.local>
2026-04-29 15:01:11 +0200

    ajuste filtros catalogo anime

--- pages/catalogo-animes.php
+++ pages/catalogo-animes.php
@@ -1,12 +1,54 @@
 <?php
 // con esto conectamos la base de datos a la pagina principal
 require_once __DIR__ . '/../includes/conexion.php';
+require_once __DIR__ . '/../includes/session.php';
+
+
+// llamamos a Jikan 
+$respuesta = file_get_contents("https://api.jikan.moe/v4/anime?status=airing&order_by=score&sort=desc&limit=20");
+$datos = json_decode($respuesta, true);
+$animes_emision = $datos['data'];
+
+// var_dump($animes_emision);
+
 // cargamos el header y el navbar en la pagina principal
 include __DIR__ . '/../includes/header.php';
 ?>
 
 <main>
     <h1>Catalogo de Animes</h1>
+
+        <div class="filtros">
+            <label for="buscar">Buscar:</label>
+            <input type="text">
+            <button type="button" class="filtro-btn" data-filtro="genero">Genero</button>
+            <button type="button" class="filtro-btn" data-filtro="estado">Estado</button>
+            <button type="button" class="filtro-btn" data-filtro="ano">Año</button>
+            <select type="button" class="filtro-btn" data-filtro="orden" placeholder="Orden por">
+                <option value="">Recientemente actualizados</option>
+                <option value="">Recientemente añadidos</option>
+                <option value="">Nombre A-Z</option>
+                <option value="">Calificacion</option>
+            </select>
+            <select type="button" class="filtro-btn" data-filtro="formato" placeholder="Formato">
+                <option value="">serie</option>
+                <option value="">pelicula</option>
+                <option value="">ova</option>
+                <option value="">especial</option>
+            </select>
+        </div>
+        <div class="grid-lista">
+        <?php foreach ($animes_emision as $anime): ?>
+            <div class="card-lista" data-tipo="<?= $anime['type'] ?>" data-estado="<?= $anime['status'] ?>">
+                <a href="/mywatchlist/pages/detalle.php?id=<?= $anime['mal_id'] ?>&tipo=<?= $anime['type'] ?>">
+                    <img src="<?= $anime['images']['jpg']['large_image_url'] ?>" alt="">
+                </a>
+                <h3><?= $anime['title'] ?></h3>
+                <p>Estado: <?= $anime['status'] ?></p>
+                <p>Progreso: <?= $anime['episodes'] ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </main>
 
 <?php
